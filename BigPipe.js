cb57b8cbacb5979ef6137e011edb97feb4328c0e
Kflash <kflash123@gmail.com>
2013-08-12 10:45:20 +0800

    Updated to version 1.5
    Updated the bigPipe to version 1.5. Major changes have been taken, and
    some bugs corrected. Script is also faster now. It takes only 348 ms
    before all paglets is fully loaded with css and javascript code. No
    cache in backend, so I guess if you use cache, it will speed up even
    more.

--- BigPipe.js
+++ BigPipe.js
@@ -3,21 +3,21 @@
 */
 
 BigPipe = function(doc) {
-	
-			var 
+
 	
 	/* Pagelet definition */	
 
-	PageLet = function(data, domInserted) {
-        var remainingCss = 0,
-            fragment, loadedcss = [],
-		inArray = function (array, filename) {
-            for (var i = array.length; i--;) 
-                if (array[i] == filename) return -1;
-				return !0;
-        },
-		 loadCss = function () {  // Attaches a CSS resource to this Pagelet
+	function PageLet(data, domInserted) {
+			 // Inject html
+	    function insertDom() {
+            console.log("Injected content for pagelet " + data.id);
+            fragment.innerHTML = data.content;
+			// Inject JS
+            domInserted()
+        }
+		 function loadCss () {  // Attaches a CSS resource to this Pagelet
             if (data.css && 0 !== data.css.length) {
+
                 console.log("Loading CSS for pagelet " + data.id);
                 remainingCss = data.css.length;
                 for (var i = remainingCss; i--;) {
@@ -28,74 +28,70 @@ BigPipe = function(doc) {
                         loadedcss.push(data.css[i]) // flag css file as 'loaded'
                     }
                 }
-            } else {
-                insertDom()
-            }
-        }, // Inject html
-         insertDom = function () {
-            console.log("Inserting content for pagelet " + data.id);
-            fragment.innerHTML = data.content;
-			// Inject JS
-            domInserted()
+            } else  insertDom()
+        }
+		function inArray (array, filename) {
+            for (var i = array.length; i--;) 
+                if (array[i] == filename) return -1;
+			return !0;
+        }
+        var remainingCss = 0,
+            fragment, loadedcss = [];
 
-        },  // Attaches a JS resource to this Pagelet.
-  			prepareDom = function () {
+		return {
+
+		  // Attaches a JS resource to this Pagelet.
+  			prepareDom: function () {
             fragment = doc.getElementById(data.id);
             console.log("Hide content for pagelet " + data.id);
             fragment.style.display = "none";
             loadCss()
         },
-
-	     loadJs = function () {
+	     loadJs: function () {
             if (!data.js) return;
              //load js
             console.log("Loading JS for pagelet " + data.id);
 			var scripts = doc.getElementsByTagName("script");
-            for (var i = data.js.length; i--;) {
+            for (var i = 0, len = data.js.length; i < len; i++) {
 				// If someone accidently add two of the same JS files to one paglet, we only load one...:
 				if(scripts[i].src == data.js) return;	
                 Loader.loadJs(data.js[i])
             }
-        };
-        return {
-            prepareDom: prepareDom,
-            loadJs: loadJs
         }
-    },
-	  Loader = function () {
-        var head = doc.getElementsByTagName("head")[0],
-			removeJavascriptFromDom = function(url) {  // Remove inserted JS script from DOM	
-				 if (head && script.parentNode)  head.parentNode.removeChild(url); 
-			},
-            loadJs = function (url, cb) {   // Inject JS in document...:
-                var script = doc.createElement("script");
-				script.async = true; // Required for FireFox 3.6 / Opera async loading.
-                script.type = "text/javascript";
-                var loaded = false,
+    }
+  }
+	var Loader = function () {
 
-            	    loadFunction = function () {
-                        if (loaded) return; // If allready loaded, nothing to do...:	 
-						console.log("loaded " + url);
-						loaded = true;
-						cb && cb();
-                   };
+	function browser() {
+
+            if (!cachedBrowser) {
+                var ua = navigator.userAgent.toLowerCase(), match = /(chrome)[ \/]([\w.]+)/.exec( ua ) || /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
+		/(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||/(msie) ([\w.]+)/.exec( ua ) || ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||	[];
+               cachedBrowser = match[1]
+            }
+		
+            return cachedBrowser
+        }
+       var head = doc.getElementsByTagName("head")[0] || doc.getElementsByTagName('body')[0], // Just to be sure when it comes to old browsers
+		   cachedBrowser;
+	return {		   
+            loadJs: function (url, cb) {   // Inject JS in document...:
+                var script = doc.createElement("script");
+				script.async = !0; // Required for FireFox 3.6 / Opera async loading.
+                script.type = "text/javascript";
+                var loaded = !1;
 
                // Real browsers
-			    script.onload = loadFunction;
+			    script.onload = function () {
+					 loaded || (console.log("loaded " + url), loaded = !0, cb && cb())
+                };
 	
 		      // Fall-back for older IE versions, they do not support the onload event on the script tag 
 	
 	           script.onreadystatechange = function() {
-					
-					if (!loaded && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete")) {
-						script.onerror = script.onload = script.onreadystatechange = null;
-					    console.log("loaded " + url);
-                       	loaded = true;
-						
-						// Handle memory leak in IE
-
-		                if (head && script.parentNode) head.removeChild(script);
-					 }					
+				
+			 loaded || this.readyState && "loaded" !== this.readyState && "complete" !== this.readyState 
+					|| (script.onerror = script.onload = script.onreadystatechange = null, console.log("loaded " + url), loaded = !0, head && script.parentNode && head.removeChild(script))
 				};
 
 			// Because of a bug in IE8, the src needs to be set after the element has been added to the document.
@@ -103,22 +99,7 @@ BigPipe = function(doc) {
                 head.appendChild(script)
                 script.src = url;
             },
-			cachedBrowser,
-        	browser = function () {
-            if (!cachedBrowser) {
-                var ua = navigator.userAgent.toLowerCase(),
-match = /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
-		/(webkit)[ \/]([\w.]+)/.exec( ua ) ||
-		/(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
-		/(msie) ([\w.]+)/.exec( ua ) ||
-		ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
-		[];
-               cachedBrowser = match[1]
-            }
-		
-            return cachedBrowser
-        };
- var loadCss = function (url, fragment, cb) {
+	 loadCss: function (url, fragment, cb) {
 		var _link = doc.createElement("link");
 		_link.rel = "stylesheet";
 		_link.type = "text/css";
@@ -137,12 +118,12 @@ match = /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
 			cb();
 		}();
 		head.appendChild(_link)
-	};
-        return {
-            loadCss: loadCss,
-            loadJs: loadJs
-        }
-    }(), OnPageLoad = function (data) {
+	}
+ }
+ }();
+ return {
+ 	
+	OnPageLoad: function (data) {
 		
 		    var 
 
@@ -151,7 +132,7 @@ match = /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
 			pagelets = [],
 
             pagelet = new PageLet(data, function () { // Load the js files for the pagelets..:
-               for (var i = pagelets.length; i--;) {
+             for (var i = pagelets.length; i--;) {
                     pagelets[i].loadJs()
                 }
             });
@@ -161,8 +142,6 @@ match = /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
         pagelets.push(pagelet); 
         pagelet.prepareDom(); // Hide all pagelets until css stylesheet is loaded.
          
-    };
-    return {
-        OnPageLoad: OnPageLoad
     }
+ }
 }(document);
