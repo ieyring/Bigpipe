9277a2f2af88613dd69ea3db70cfc3a21818ded0
Kflash <kflash123@gmail.com>
2013-08-14 13:32:19 +0800

    Re-uploaded v.2.0
    I had to re-upload v. 2.0 with changes because the first one was not
    working at all. Everything is fine now

--- BigPipe.js
+++ BigPipe.js
@@ -3,6 +3,7 @@
 */
 
 BigPipe = function(doc) {
+
 	/* Pagelet definition */	
 
 	function PageLet(data, domInserted) {
@@ -34,7 +35,6 @@ for (var i = remainingCss = data.css.length; i--;) inArray(loadedcss, data.css[i
 
 		  // Attaches a JS resource to this Pagelet.
   			prepareDom: function () {
-//				if( ! data.id) throw new Error("Something terrible happend."); return;
             fragment = doc.getElementById(data.id);
             console.log("Hide content for pagelet " + data.id);
             fragment.style.display = "none";
@@ -44,14 +44,15 @@ for (var i = remainingCss = data.css.length; i--;) inArray(loadedcss, data.css[i
             if (!data.js) return;
              //load js
             console.log("Loading JS for pagelet " + data.id);
-            for (var scripts = doc.getElementsByTagName("script"), JSValidate = /js$/i, i = data.js.length; i--;) 
-				if( JSValidate.test( scripts[i].src ) ) { 			// Make sure this really is a javascript file
-					if(scripts[i].src == data.js) return;	 	// If someone accidently add two of the same JS files to one paglet, we only load one...:
-					Loader.loadJs(data.js[i])
-	            }
-        	}
-	    }
-	}
+			var scripts = doc.getElementsByTagName("script");
+            for (var i = data.js.length, JSValidate = /js$/i; i--;) {
+				// If someone accidently add two of the same JS files to one paglet, we only load one...:
+				if(scripts[i].src == data.js) return;	
+                Loader.loadJs(data.js[i])
+            }
+        }
+    }
+  }
 	var Loader = function () {
 
 	function browser() {
@@ -59,7 +60,7 @@ for (var i = remainingCss = data.css.length; i--;) inArray(loadedcss, data.css[i
             if (!cachedBrowser) {
                 var ua = navigator.userAgent.toLowerCase(), match = /(chrome)[ \/]([\w.]+)/.exec( ua ) || /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
 		/(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||/(msie) ([\w.]+)/.exec( ua ) || ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||	[];
-               cachedBrowser = match[1];
+               cachedBrowser = match[1]
             }
 		
             return cachedBrowser
@@ -73,20 +74,19 @@ for (var i = remainingCss = data.css.length; i--;) inArray(loadedcss, data.css[i
                 script.type = "text/javascript";
                 var loaded = !1;
 
-
-			// Hack for older Opera browsers. Some of them fires load event multiple times, even when the DOM is not ready yet.
-			// This have no impact on the newest Opera browsers, because they share the same engine as Chrome.
-			
-			if("opera" == browser() && this.readyState && this.readyState != 'complete') { // Affected: Opera Opera 9.26, 10.00
-				return;
-			}
-
-              // Real browsers
+    		   
+	// Hack for older Opera browsers. Some of them fires load event multiple times, even when the DOM is not ready yet.
+      // This have no impact on the newest Opera browsers, because they share the same engine as Chrome.
+      
+      if("opera" == browser() && this.readyState && this.readyState != 'complete') { // Affected: Opera Opera 9.26, 10.00
+        return;
+      }			   
+          // Real browsers		   
 			    script.onload = function () {
 					 loaded || (console.log("loaded " + url), loaded = !0, cb && cb())
                 };
 	
-		      // Fall-back for older IE versions ( IE 6 & 7), they do not support the onload event on the script tag 
+		      // Fall-back for older IE versions ( IE 6 & 7), they do not support the onload event on the script tag  
 	
 	           script.onreadystatechange = function() {
 				
@@ -124,6 +124,9 @@ for (var i = remainingCss = data.css.length; i--;) inArray(loadedcss, data.css[i
  return {
  	
 	OnPageLoad: function (data) {
+
+	// Allways add a css file, else the code will not run
+    if(! data.css) { console.log("Injection canceled. No CSS defined."); return -1; }		
 		
 		if (window.removeEventListener) window.removeEventListener("DOMContentLoaded", BigPipe.OnPageLoad, !1), window.removeEventListener("load", BigPipe.OnPageLoad, !1);
             else {
@@ -136,9 +139,6 @@ for (var i = remainingCss = data.css.length; i--;) inArray(loadedcss, data.css[i
 	},
 
 	run: function(data) {
-		
-		// Allways add a css file, else the code will not run
-		if(! data.css) { console.log("Injection canceled. No CSS defined."); return -1; }
 		
 		    var 
 
