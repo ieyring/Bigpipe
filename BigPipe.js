d9a06a72b29c03bab3d1bf6f5281ae49a12492e3
KFlash <kflash123@gmail.com>
2013-09-10 09:50:25 +0800

    Speed improvments

--- BigPipe.js
+++ BigPipe.js
@@ -1,16 +1,14 @@
-/* Bigpipe - version 3.6
+/* Bigpipe - version 3.7
 Kenny F. 2013
 */
 var BigPipe = function (doc) {
 
-    "use_strict";
     /**
      * Configure the Pagelet.
      *
      * @param {data} contain all paglet data sent from server.
      * @param {fragment} HTML injected div fragment
      */
-
 
     function PagLet(data, fragment, c) {
         function inArray(a, b) {
@@ -35,7 +33,7 @@ var BigPipe = function (doc) {
                         // If it fail, it will automaticly try to re-load the stylesheet because it's not in the array
 
                         inArray(loadedcss, k) || Loader.loadCss(k, function (a, c) {
-                            a ? (loadedcss.push(k), loadedcss.length == b && (fragment.style.display = "block")) : console.log("Warning! An error occured during injection of CSS file: " + k)
+                            a ? (loadedcss.push(k), loadedcss.length == b && (fragment.style.display = "block")) : alert("Error during injection of CSS file: " + k)
                             // After the last stylesheet is loaded, and we have got a positive result feedback, inject paglet data
                             // and make the paglet visible
                         });
@@ -53,18 +51,15 @@ var BigPipe = function (doc) {
                 var b = doc.getElementsByTagName("script");
                 (function h(c) {
                     var st = setTimeout(function () {
-                        b[c - 1].src != data.js && (Loader.loadJs(data.js[c - 1], function () {
-                            clearTimeout(st);
-                        }), --c && h(c))
+                        clearTimeout(st); // clear the counters	
+                        b[c - 1].src != data.js && (Loader.loadJs(data.js[c - 1], function () {}), --c && h(c))
                     }, 15)
                 })(data.js.length)
+
             }
         }
     }
     var Loader = function () {
-        var a = doc.getElementsByTagName("head")[0] || doc.getElementsByTagName("body")[0];
-        navigator.userAgent.toLowerCase();
-        var Opera = /opera/i.test(navigator.userAgent);
         return {
             loadJs: function (url, cb) {
 
@@ -76,13 +71,13 @@ var BigPipe = function (doc) {
                         f = !1;
                     script.async = true; // or false;
                     script.type = "text/javascript";
-                    script.id = "script" + Math.floor(911);
+                    script.id = "script" + ~~911;
 
 
                     // Hack for older Opera browsers. Some of them fires load event multiple times, even when the DOM is not ready yet.
                     // This have no impact on the newest Opera browsers, because they share the same engine as Chrome.
 
-                    Opera && this.readyState && "complete" != this.readyState || (script.onload = function () {
+                    /opera/i.test(navigator.userAgent) && this.readyState && "complete" != this.readyState || (script.onload = function () {
                             f || (f = !0, cb && cb())
                         }, // Fall-back for older IE versions ( IE 6 & 7), they do not support the onload event on the script tag  
                         script.onreadystatechange = function () {
@@ -95,64 +90,55 @@ var BigPipe = function (doc) {
             // Inject CSS files into the document
 
             loadCss: function (path, cb, scope) {
-                var _link = doc.createElement("link");
-                _link.href = path;
-                _link.rel = "stylesheet";
-                _link.type = "text/css";
-                var sheet, cssRules;
+                if (path.match(/css/) && "" != path) {
+                    var _link = doc.createElement("link")
+                    _win = window;
+                    _link.href = path;
+                    _link.rel = "stylesheet";
+                    _link.type = "text/css";
+                    var sheet, cssRules;
 
-                // get the correct properties to check for depending on the browser
+                    // get the correct properties to check for depending on the browser
 
-                "sheet" in _link ? (sheet = "sheet", cssRules = "cssRules") : (sheet = "styleSheet", cssRules = "rules");
-                var g = setInterval(function () { // start checking whether the style sheet has successfully loaded
-                    try {
-                        _link[sheet] && _link[sheet][cssRules].length && (clearInterval(g), clearTimeout(k), cb.call(scope || window, !0, _link))
-                    } catch (a) {} finally {}
-                }, 15), // how often to check if the stylesheet is loaded
-                    k = setTimeout(function () {
-                        clearInterval(g); // clear the counters
-                        clearTimeout(k);
-                        a.removeChild(_link);
-                        cb.call(scope || window, !1, _link) // fire the callback with success == true
-                    }, 1500);
-                a.appendChild(_link); // how long to wait before failing
+                    "sheet" in _link ? (sheet = "sheet", cssRules = "cssRules") : (sheet = "styleSheet", cssRules = "rules");
+                    var g = setInterval(function () { // start checking whether the style sheet has successfully loaded
+                        try {
+                            _link[sheet] && _link[sheet][cssRules].length && (clearInterval(g), clearTimeout(k), cb.call(scope || _win, !0, _link))
+                        } catch (a) {} finally {}
+                    }, 10), // how often to check if the stylesheet is loaded
+                        k = setTimeout(function () {
+                            clearInterval(g); // clear the counters
+                            clearTimeout(k);
+                            a.removeChild(_link);
+                            cb.call(scope || _win, !1, _link) // fire the callback with success == true
+                        }, 1500);
+                    (doc.getElementsByTagName("head")[0] || doc.getElementsByTagName("body")[0]).appendChild(_link); // how long to wait before failing
 
+                }
             }
         }
     }();
     return {
         OnPageLoad: function (data) {
 
-            try {
+            // Hide the paglet until css are injected
 
-                // Hack for IE9 and older IE versions, to avoid the console.log problem
+            if ("string" == typeof data.id && (fragment = doc.getElementById(data.id) || ""), "" !== fragment) {
+                fragment.style.display = "none";
+                var e = [],
+                    pagelet = new PagLet(data, fragment, function () {
+                        if (data.js) {
+                            for (var b = e.length; b--;) {
+                                e[b].loadJs();
+                            }
+                        }
+                    });
+                e.push(pagelet);
+                pagelet.loadCss();
 
-                if (window.console || (console = {
-                    log: function () {}
-                }), data.css) { // Allways add a css file, else we prevent the code from running
-
-                    // Hide the paglet until css are injected
-
-                    if ("string" == typeof data.id && (fragment = doc.getElementById(data.id.toLowerCase()) || ""), "" !== fragment) {
-                        fragment.style.display = "none";
-                        var e = [],
-                            pagelet = new PagLet(data, fragment, function () {
-                                var b = e.length;
-                                while (b--) {
-                                    0 < data.js.length && e[b].loadJs();
-                                }
-                                b = null;
-
-                            });
-                        e.push(pagelet);
-                        pagelet.loadCss();
-                    }
-                } else {
-                    window.location.href = data.onError
-                }
-            } catch (g) {
-                window.location.href = data.onError
+            } else {
+                (doc.location || window.location).href = data.onError;
             }
         }
     }
-}(document);
+}(document);f
